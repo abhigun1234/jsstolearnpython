7d65920783780fd1a160c3261c6ee76b89e06069
Abhishek <abhi.ghosh27feb@gmail.com>
2021-01-02 10:30:22 +0530

    function demo

--- weekendbatch/function/function demo.py
+++ weekendbatch/function/function demo.py
@@ -1,13 +1,35 @@
-# function defination
-def hello(year):
-    print('argumnt',year)
-    print("hello every on welcome to the year ",year)
-    if year==2020:
-        print('this was a corona year')
-    else:
-        print('corona is over now enjoy')
+# # function defination
+# def hello(year):
+#     print('argumnt',year)
+#     print("hello every on welchonome to the year ",year)
+#     if year==2020:
+#         print('this was a corona year')
+#     else:
+#         print('corona is over now enjoy')
+#
+# # function calling
+#
+# year=int(input('enter the year'))
+# hello(year)
+# def offer(season):
+#     if season== 'diwali':
+#         print("20% Discount")
+#     else:
+#         print("25% Discount")
 
-# function calling
+# stusent list add data through function
+student_list=[]
+studetData={}
 
-year=int(input('enter the year'))
-hello(year)
+def addStudenData(name,address,phone_no):
+    student_list.append(name)
+    return student_list
+
+name=input('enter student name')
+address=input('enter address')
+phone_no=input('enter phone no')
+student_list=addStudenData(name,address,phone_no)
+studentData={"studentsta":student_list}
+print('studentData',studentData)
+
+
