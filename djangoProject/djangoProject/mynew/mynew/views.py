1da148815f68f65f1d7d397f6a203196b7b2efc5
Abhishek <abhi.ghosh27feb@gmail.com>
2021-02-07 11:15:25 +0530

    add

--- djangoProject/djangoProject/mynew/mynew/views.py
+++ djangoProject/djangoProject/mynew/mynew/views.py
@@ -4,5 +4,6 @@ def home(request):
     return render(request,'hello.html',{'titles':'hello','link':'http://youtube.com'})
 def aboutUs(request):
     return  render(request,'aboutus.html',{'titles':'hello','link':'http://youtube.com'})
-
+def contactUs(request):
+    return  render(request,'aboutus.html',{'titles':'hello','link':'http://youtube.com'})
 
